627358c27a684848f7547b9dc0828c442b5d4378
Fatih Giris <girisfth@gmail.com>
2021-08-02 23:41:27 +0200

    - Update animation apis list

--- app/src/main/java/dev/fgiris/composeanimations/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/fgiris/composeanimations/ui/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package dev.fgiris.composeanimations.ui.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
@@ -8,22 +9,51 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dev.fgiris.composeanimations.MainActions
 import dev.fgiris.composeanimations.data.AnimationApiType
 
 private val highLevelAnimationApis = listOf(
-    Pair("AnimatedVisibility", AnimationApiType.ANIMATED_VISIBILITY),
-    Pair("AnimatedContent", AnimationApiType.ANIMATED_CONTENT)
+    Triple(
+        "AnimatedVisibility",
+        AnimationApiType.ANIMATED_VISIBILITY,
+        "Animate the visibility of a composable"
+    ),
+    Triple(
+        "AnimatedContent",
+        AnimationApiType.ANIMATED_CONTENT,
+        "Animate the whole content of a composable"
+    )
 )
 
 private val lowLevelAnimationApis = listOf(
-    Pair("animate*AsState", AnimationApiType.ANIMATE_AS_STATE),
-    Pair("updateTransition", AnimationApiType.UPDATE_TRANSITION),
-    Pair("rememberInfiniteTransition", AnimationApiType.REMEMBER_INFINITE),
-    Pair("Animatable", AnimationApiType.ANIMATABLE),
-    Pair("TargetBasedAnimation", AnimationApiType.TARGET_BASED_ANIMATION),
-    Pair("DecayAnimation", AnimationApiType.DECAY_ANIMATION)
+    Triple(
+        "animate*AsState",
+        AnimationApiType.ANIMATE_AS_STATE,
+        "Animate using common data types like Int, Float etc. as state"
+    ),
+    Triple(
+        "updateTransition",
+        AnimationApiType.UPDATE_TRANSITION,
+        "Animate using a custom target state"
+    ),
+    Triple("rememberInfiniteTransition", AnimationApiType.REMEMBER_INFINITE, "Infinite animations"),
+    Triple(
+        "Animatable",
+        AnimationApiType.ANIMATABLE,
+        "Animations which are not dependent to composition"
+    ),
+    Triple(
+        "TargetBasedAnimation",
+        AnimationApiType.TARGET_BASED_ANIMATION,
+        "Control the animation timing"
+    ),
+    Triple(
+        "DecayAnimation",
+        AnimationApiType.DECAY_ANIMATION,
+        "Stateless animations with given play time"
+    )
 )
 
 @ExperimentalFoundationApi
@@ -43,8 +73,8 @@ fun HomeScreen(navActions: MainActions) {
 @ExperimentalFoundationApi
 @Composable
 fun HomeScreen(
-    highLevelAnimationApis: List<Pair<String, AnimationApiType>>,
-    lowLevelAnimationApis: List<Pair<String, AnimationApiType>>,
+    highLevelAnimationApis: List<Triple<String, AnimationApiType, String>>,
+    lowLevelAnimationApis: List<Triple<String, AnimationApiType, String>>,
     navActions: MainActions
 ) {
     LazyColumn(
@@ -75,26 +105,44 @@ fun HomeScreen(
 fun AnimationApisList(
     scope: LazyListScope,
     header: String,
-    apis: List<Pair<String, AnimationApiType>>,
+    apis: List<Triple<String, AnimationApiType, String>>,
     navActions: MainActions
 ) {
     scope.apply {
         stickyHeader {
             Text(
                 text = header,
-                style = MaterialTheme.typography.h5
+                style = MaterialTheme.typography.subtitle1.copy(
+                    color = Color(0xFF757575)
+                )
             )
         }
 
         item { AnimationListDivider() }
 
         items(apis) { item ->
-            Button(
-                onClick = {
-                    navActions.navigateToAnimationDetails(item.second)
-                }
+            Column(
+                modifier = Modifier
+                    .padding(bottom = 4.dp)
+                    .clickable { navActions.navigateToAnimationDetails(item.second) }
             ) {
-                Text(text = item.first)
+                Row {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+                        text = item.first,
+                        style = MaterialTheme.typography.body1
+                    )
+                }
+                Row {
+                    Text(
+                        text = item.third,
+                        style = MaterialTheme.typography.body2.copy(
+                            color = Color(0xFF757575)
+                        )
+                    )
+                }
             }
         }
     }
